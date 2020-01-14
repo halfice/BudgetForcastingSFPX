10e48ddd17a03222b8dac96f4a300a90387bd323
Abdul Aziz Farooqi <IA4188@internal.adsic.abudhabi.ae>
2020-01-14 16:29:28 +0400

    finializing code

--- src/webparts/arabicformwebpart/components/IArabicformwebpartProps.ts
+++ src/webparts/arabicformwebpart/components/IArabicformwebpartProps.ts
@@ -2,6 +2,8 @@ import { SPHttpClient, } from '@microsoft/sp-http';
 import { Context } from 'react';
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { PageContext } from '@microsoft/sp-page-context';
+import { DetailsList, DetailsListLayoutMode, Selection, SelectionMode, IColumn } from 'office-ui-fabric-react/lib/DetailsList';
+
 export interface IArabicformwebpartProps {
   description: string;
   greetings: string;
@@ -28,7 +30,12 @@ export interface IArabicformwebpartProps {
   BudgetForcasting:Array<object>;
   Remarks:string;
   MonthlyDeliveredAmount:string;
-  
+  BalanceForcastTotal:Number;
+  BalanceDeliverTotal:Number;
+  Monitoritems: Array<object>;
+  MonitorColumns: IColumn[];
+  MonitorIndex:number;
+  showPanel: boolean;
   
 
 
